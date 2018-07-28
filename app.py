70c7d4fc71a40bc33d68c76b5a6cec47bd38c0c8
wsxdrorange <rick.zhang@yahoo.com>
2018-07-27 19:31:12 -0500

    changes

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for, request
+import main
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,19 @@
-/**
- * Created by rickz on 7/27/2018.
- */
+var index = 0;
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,change = to - start,currentTime = 0,increment = 20;
+    function animateScroll()
+    {
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {setTimeout(animateScroll, increment);}
+    };
+    animateScroll();
+}
+function runSearch(e)
+{
+    if(e.keyCode = 13){
+        
+    }
+    scrollTo(document.body, screen.height -90, 1250);
+}
