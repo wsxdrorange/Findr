80fed97f65b0f09f1de156b890848a0ae4f3b86d
wsxdrorange <rick.zhang@yahoo.com>
2018-07-29 03:59:05 -0500

    Major changes

--- static/js/home.js
+++ static/js/home.js
@@ -1,23 +1,74 @@
 var index = 0;
 
-function scrollTo(element, to, duration) {
-    var start = element.scrollTop,change = to - start,currentTime = 0,increment = 20;
-    function animateScroll()
-    {
-        currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
-        if(currentTime < duration) {setTimeout(animateScroll, increment);}
+var defaultURLs = ["https://github.com/", "https://www.youtube.com/user/", "https://twitter.com/", "https://www.instagram.com/", "http://.tumblr.com/"];
+var statusBubbleSelectors = ["gitStatus", "youtubeStatus", "twitterStatus", "instaStatus", "tumblrStatus"];
+var currentUser;
+var urls = [];
+
+function editURLs(username) { //adds username to each default url into array urls
+    var i;
+    for (i = 0; i < 4; i++) {
+        urls.push(defaultURLs[i] + username);
     }
-    animateScroll();
+    urls[4] = "http://" + username + ".tumblr.com/";
 }
+
+function editStatusBubbles() {
+    var i;
+    for (i = 0; i < urls.length; i++) {
+        document.getElementById(statusBubbleSelectors[i]).href = urls[i];
+        if (UrlExists(urls[i])) {
+            document.getElementById(statusBubbleSelectors[i]).style.background = '#26a69a';
+        }
+    }
+}
+
+function UrlExists(url) {
+    var request;
+    if (window.XMLHttpRequest)
+        request = new XMLHttpRequest();
+    else
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    request.open('GET', url, false);
+    request.send(); // there will be a 'pause' here until the response to come.
+    // the object request will be actually modified
+    console.log(request.status);
+    if (request.status === 404) {
+        return false;
+    }
+    return true;
+}
+
 window.onload = function () {
-    document.getElementById("user").onkeydown = function(event) {
-        if(event.keyCode === 13) {
+    document.getElementById("user").onkeydown = function (event) {
+        if (event.keyCode === 13) {
             event.preventDefault();
-            $('html, body').animate({
+
+
+            currentUser = document.getElementById("user").value;
+            editURLs(currentUser);
+            for (var i = 0; i < urls.length; i++) {
+                console.log(urls[i]);
+            }
+            editStatusBubbles();
+
+            $('html, body').animate({ //smooth scroll down
                 scrollTop: $("#loadout").offset().top
             }, 1000);
         }
+    }
+
+    document.getElementById("return").onclick = function (){
+        $('html, body').animate({ //smooth scroll up to search
+                scrollTop: $(".video-container").offset().top
+            }, 1000);
+    }
+
+    document.getElementById("myInfo").onclick = function (){
+        $('html, body').animate({
+                scrollTop: $("#loadout").offset().top
+            }, 1000);
+        editURLs("wsxdrorange");
+        editStatusBubbles();
     }
 };
