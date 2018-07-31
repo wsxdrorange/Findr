d78c6b51d42b379039461a9d924f31d49ca3e278
wsxdrorange <rick.zhang@yahoo.com>
2018-07-30 23:28:49 -0500

    Major changes

--- static/js/home.js
+++ static/js/home.js
@@ -1,10 +1,5 @@
 var index = 0;
 
-$("user").click(function() {
-   $('html, body').animate({
-       scrollTop: $("").offset().top
-   }, 2000);
-});
 function scrollTo(element, to, duration) {
     var start = element.scrollTop,change = to - start,currentTime = 0,increment = 20;
     function animateScroll()
@@ -13,12 +8,16 @@ function scrollTo(element, to, duration) {
         var val = Math.easeInOutQuad(currentTime, start, change, duration);
         element.scrollTop = val;
         if(currentTime < duration) {setTimeout(animateScroll, increment);}
-    };
+    }
     animateScroll();
 }
-function runSearch(e)
-{
-    if(e.keyCode == 13) {
-        scrollTo(document.body, screen.height - 90, 1250);
+window.onload = function () {
+    document.getElementById("user").onkeydown = function(event) {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#loadout").offset().top
+            }, 1000);
+        }
     }
-}
+};
